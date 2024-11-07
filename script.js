6587b127b618e033d4959babf83c29462c7bb61b
Muhamad Riza Pahlevi <134340338+yumsha@users.noreply.github.com>
2024-11-08 00:26:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const pageContent = document.querySelector(".anim");
+    if (pageContent) {
+        pageContent.classList.add("fadeIn");
+    }
+});
